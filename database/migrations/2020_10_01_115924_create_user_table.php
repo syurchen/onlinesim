e6646677f881215c184761b4ee5320680904a908
Sergey Yurchenko <yurchenko@ratio.bz>
2020-10-01 16:24:59 +0300

    feat: added migrations

---
+++ database/migrations/2020_10_01_115924_create_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('second_name');
+            $table->string('country');
+            $table->string('phone');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
