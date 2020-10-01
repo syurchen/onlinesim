93522918a4c61bb978b4c7903883b57d4479d2fa
Sergey Yurchenko <yurchenko@ratio.bz>
2020-10-01 16:23:56 +0300

    feat: added Repository and search method

--- app/Http/Controllers/DbApiController.php
+++ app/Http/Controllers/DbApiController.php
@@ -3,26 +3,38 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Repositories\UserRepository;
+use App\Models\User;
 
 class DbApiController extends Controller
 {
+
+    protected $users;
+
+    public function __construct(UserRepository $users){
+
+        $this->users = $users;
+    }
+
     /**
      * Get data from DB.
      *
      * @return Json
      */
     public function getByRegex(
-        $id = NULL,
-        $firstName = NULL,
-        $secondName = NULL,
-        $country = NULL,
-        $phone = NULL,
-        $company = NULL
+        Request $request
     )
     {
-        return response()->json([
-            'name' => 'Abigail',
-            'state' => 'CA',
-        ]);
+        $input = $request->all();
+        $id = $input['id'] ?? NULL;
+        $first_name = $input['first_name'] ?? NULL;
+        $second_name = $input['second_name'] ?? NULL;
+        $country = $input['country'] ?? NULL;
+        $phone = $input['phone'] ?? NULL;
+        $company = $input['company'] ?? NULL;
+
+        $users = $this->users->getUsersByRegex($id, $first_name, $second_name, $country, $phone, $company);
+        return response()->json($users);
     }
 }
